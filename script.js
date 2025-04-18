4a257cf741107e8b8c4be75721abf13e4e5caf21
LIFE CODING WITH JAVA <127834681+6206472735rahul@users.noreply.github.com>
2025-04-18 14:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let lowerStr= s1.toLowerCase(s1);
+	let lowerSubs= s2.toLowerCase(s2);
+  let firstIdx= lowerStr.indexOf(lowerSubs);
+	return firstIdx;
+	
 }
 
 // Please do not change the code below
